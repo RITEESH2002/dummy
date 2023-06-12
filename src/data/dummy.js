5cc3808c4ada898f5bb8877b98076e455e15fcfd
Riteesh Pai <riteeshpai2002@gmail.com>
2023-06-12 09:18:03 +0530

    Dummy

---
+++ src/data/dummy.js
@@ -0,0 +1,88 @@
+import { AiOutlineCalendar, AiOutlineShoppingCart, AiOutlineAreaChart, AiOutlineBarChart, AiOutlineStock } from 'react-icons/ai';
+import { RiContactsLine, RiStockLine } from 'react-icons/ri';
+import { BiColorFill } from 'react-icons/bi';
+import { GiLouvrePyramid } from 'react-icons/gi';
+import { IoMdContacts } from 'react-icons/io';
+import { FiShoppingBag, FiEdit, FiPieChart, FiBarChart, FiCreditCard, FiStar, FiShoppingCart } from 'react-icons/fi';
+import { BsKanban, BsBarChart, BsBoxSeam, BsCurrencyDollar, BsShield, BsChatLeft } from 'react-icons/bs';
+
+export const links = [
+  {
+    title: 'Dashboard',
+    links: [
+      {
+        name: 'BANKING',
+        icon: <FiShoppingBag />,
+      },
+    ],
+  },
+
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'orders',
+        icon: <AiOutlineShoppingCart />,
+      },
+      {
+        name: 'USERS',
+        icon: <IoMdContacts />,
+      },
+      {
+        name: 'ISSUES',
+        icon: <RiContactsLine />,
+      },
+    ],
+  },
+  {
+    title: 'Apps',
+    links: [
+      {
+        name: 'calendar',
+        icon: <AiOutlineCalendar />,
+      },
+      {
+        name: 'kanban',
+        icon: <BsKanban />,
+      },
+      {
+        name: 'editor',
+        icon: <FiEdit />,
+      },
+    ],
+  },
+  {
+    title: 'Charts',
+    links: [
+      {
+        name: 'line',
+        icon: <AiOutlineStock />,
+      },
+      {
+        name: 'area',
+        icon: <AiOutlineAreaChart />,
+      },
+
+      {
+        name: 'bar',
+        icon: <AiOutlineBarChart />,
+      },
+      {
+        name: 'financial',
+        icon: <RiStockLine />,
+      },
+      {
+        name: 'color-mapping',
+        icon: <BsBarChart />,
+      },
+      {
+        name: 'pyramid',
+        icon: <GiLouvrePyramid />,
+      },
+      {
+        name: 'stacked',
+        icon: <AiOutlineBarChart />,
+      },
+    ],
+  },
+];
